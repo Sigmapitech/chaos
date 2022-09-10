f8ce42dd805c8164a65d7fc9cd3a8494d3290df2
sigmanificient <edhyjox@gmail.com>
2022-09-10 07:42:42 +0200

    :sparkles: Add chaos base __init__ & __main__

---
+++ chaos/__init__.py
@@ -0,0 +1,2 @@
+__author__ = "Sigmanificient"
+__version__ = "0.0.0"
---
+++ chaos/__main__.py
@@ -0,0 +1,9 @@
+from . import __version__
+
+
+def main():
+    print(f'Running Chaos v{__version__}')
+
+
+if __name__ == '__main__':
+    main()
