d350db0ba7fe68247dec29be008e3a270ecf79a8
Sigmanificient <edhyjox@gmail.com>
2022-10-01 19:22:41 +0200

    :fire: Reset project due to unexpected changes

--- chaos/__main__.py
+++ chaos/__main__.py
@@ -1,20 +1,9 @@
-import sys
-
-from .syntax.parser import Parser
 from . import __version__
 
 
 def main():
     print(f'Running Chaos v{__version__}')
-    if len(sys.argv) < 2:
-        print("Please provide a program as string.")
-        return
-
-    print(sys.argv[1])
-    parser = Parser(sys.argv[1])
-    print("Parsing AST...")
-    return parser.parse()
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
